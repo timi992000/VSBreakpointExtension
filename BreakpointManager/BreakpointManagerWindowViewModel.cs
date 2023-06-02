417ea6f5821cfa5d775585ea037bd25b8aa9deb1
timi992000 <77628993+timi992000@users.noreply.github.com>
2023-06-02 15:32:14 +0200

    Set property breakpoints to get/set

--- BreakpointManager/BreakpointManagerWindowViewModel.cs
+++ BreakpointManager/BreakpointManagerWindowViewModel.cs
@@ -5,85 +5,87 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace BreakpointManager
 {
-  public class BreakpointManagerWindowViewModel : ViewModelBase
-  {
-    private SolutionEvents m_SolutionEvents;
-    private DocumentEvents m_DocumentEvents;
-    public BreakpointManagerWindowViewModel()
-    {
-      __Initialize();
-    }
+	public class BreakpointManagerWindowViewModel : ViewModelBase
+	{
+		private SolutionEvents m_SolutionEvents;
+		private DocumentEvents m_DocumentEvents;
+		public BreakpointManagerWindowViewModel()
+		{
+			__Initialize();
+		}
 
-    public string DocumentName => PackageContext.Instance.CurrentDocument?.Name;
-    public string SolutionName => PackageContext.Instance.CurrentSolution?.FileName;
-    public SolutionEvents SolutionEvents => m_SolutionEvents;
+		public string DocumentName => PackageContext.Instance.CurrentDocument?.Name;
+		public string SolutionName => PackageContext.Instance.CurrentSolution?.FileName;
+		public SolutionEvents SolutionEvents => m_SolutionEvents;
 
-    public void RefreshCurrentState()
-    {
-      OnPropertyChanged(nameof(DocumentName));
-    }
+		public void RefreshCurrentState()
+		{
+			OnPropertyChanged(nameof(DocumentName));
+		}
 
-    public void SetBreakpointsToAllMethods()
-    {
-      ThreadHelper.ThrowIfNotOnUIThread();
-      TextSelection ts = PackageContext.Instance.DTE.ActiveDocument.Selection as TextSelection;
-      if (ts == null)
-        return;
+		public void SetBreakpointsToAllMethods()
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+			TextSelection ts = PackageContext.Instance.DTE.ActiveDocument.Selection as TextSelection;
+			if (ts == null)
+				return;
 
-      CodeClass c = ts.ActivePoint.CodeElement[vsCMElement.vsCMElementClass] as CodeClass;
-      if (c == null)
-        return;
+			CodeClass c = ts.ActivePoint.CodeElement[vsCMElement.vsCMElementClass] as CodeClass;
+			if (c == null)
+				return;
 
-      foreach (CodeElement e in c.Members)
-      {
-        if (e.Kind == vsCMElement.vsCMElementFunction)
-        {
-          TextPoint p = (e as CodeFunction).GetStartPoint();
-          PackageContext.Instance.DTE.Debugger.Breakpoints.Add("", p.Parent.Parent.FullName, p.Line);
-        }
-      }
-    }
+			foreach (CodeElement e in c.Members)
+			{
+				if (e.Kind == vsCMElement.vsCMElementFunction)
+				{
+					TextPoint p = (e as CodeFunction).GetStartPoint();
+					PackageContext.Instance.DTE.Debugger.Breakpoints.Add("", p.Parent.Parent.FullName, p.Line);
+				}
+			}
+		}
 
-    public void SetBreakpointsToProperties()
-    {
-      ThreadHelper.ThrowIfNotOnUIThread();
-      TextSelection ts = PackageContext.Instance.DTE.ActiveDocument.Selection as TextSelection;
-      if (ts == null)
-        return;
+		public void SetBreakpointsToProperties()
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+			TextSelection ts = PackageContext.Instance.DTE.ActiveDocument.Selection as TextSelection;
+			if (ts == null)
+				return;
 
-      CodeClass c = ts.ActivePoint.CodeElement[vsCMElement.vsCMElementClass] as CodeClass;
-      if (c == null)
-        return;
+			CodeClass c = ts.ActivePoint.CodeElement[vsCMElement.vsCMElementClass] as CodeClass;
+			if (c == null)
+				return;
 
-      foreach (CodeElement e in c.Members)
-      {
-        if (e.Kind == vsCMElement.vsCMElementProperty)
-        {
-          TextPoint p = (e as CodeProperty).GetStartPoint();
-          PackageContext.Instance.DTE.Debugger.Breakpoints.Add("", p.Parent.Parent.FullName, p.Line);
-        }
-      }
-    }
+			foreach (CodeElement e in c.Members)
+			{
+				if (e.Kind == vsCMElement.vsCMElementProperty)
+				{
+					TextPoint p = (e as CodeProperty).Getter.StartPoint;
+					PackageContext.Instance.DTE.Debugger.Breakpoints.Add("", p.Parent.Parent.FullName, p.Line);
+					p = (e as CodeProperty).Setter.StartPoint;
+					PackageContext.Instance.DTE.Debugger.Breakpoints.Add("", p.Parent.Parent.FullName, p.Line);
+				}
+			}
+		}
 
-    private void __Initialize()
-    {
-      __AttachEvents();
-      var solution = PackageContext.Instance.ServiceProvider.GetService<SVsSolution, IVsSolution>();
-      PackageContext.Instance.Solution = solution;
-    }
+		private void __Initialize()
+		{
+			__AttachEvents();
+			var solution = PackageContext.Instance.ServiceProvider.GetService<SVsSolution, IVsSolution>();
+			PackageContext.Instance.Solution = solution;
+		}
 
-    private void __AttachEvents()
-    {
-      ThreadHelper.ThrowIfNotOnUIThread();
-      var dteSvc = PackageContext.Instance.DTE;
-      m_SolutionEvents = dteSvc.Events.SolutionEvents;
-      m_DocumentEvents = dteSvc.Events.DocumentEvents;
-      m_SolutionEvents.Opened += __SolutionOpened;
-    }
-    private void __SolutionOpened()
-    {
-      PackageContext.Instance.Solution = BreakpointManagerWindowCommand.Instance.Package.GetService<SVsSolution, IVsSolution>();
-      OnPropertyChanged(nameof(SolutionName));
-    }
-  }
+		private void __AttachEvents()
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+			var dteSvc = PackageContext.Instance.DTE;
+			m_SolutionEvents = dteSvc.Events.SolutionEvents;
+			m_DocumentEvents = dteSvc.Events.DocumentEvents;
+			m_SolutionEvents.Opened += __SolutionOpened;
+		}
+		private void __SolutionOpened()
+		{
+			PackageContext.Instance.Solution = BreakpointManagerWindowCommand.Instance.Package.GetService<SVsSolution, IVsSolution>();
+			OnPropertyChanged(nameof(SolutionName));
+		}
+	}
 }
